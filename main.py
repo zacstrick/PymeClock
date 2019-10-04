94c53fb16861208d6dabec3ed23ea1dd98970434
zacstrick <38014947+zacstrick@users.noreply.github.com>
2019-10-04 13:27:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,48 @@
 import csv
 import sys
 import datetime
-import pandas as pd
-import numpy as np
+import PySimpleGUI as sg
 
-loopty = True
-def prog() :
-      
-# input number you want to search
-  cardNumber = input('Ready for your swipe!\n')
+# here we go with the window
+layout = [[sg.Text('Swipe Your Card')],
+          [sg.InputText(size=(6,1), key='IN', focus=True)],
+          [sg.Text('', key='theName', size=(45,1))],
+          [sg.Text('', key='theTime', size=(45,1))],
+          [sg.Button('', key='RETURN', visible=False, bind_return_key=True)]]
+
+window = sg.Window('Clock!', layout)
 
 # here are the csv reader vars
-  csvItself = open('everyone-master.csv', "r")
-  parsedcsv = csv.reader(csvItself)
-  
-# this'll be the csv writer's vars but these don't work for some reason?
-  #newCSV = open("C:\Users\zstrickland.RANDSMACHINE\Documents\ClockInCardProject\ClockInPython\eweveryone.csv")
-  #writer = csv.writer(newCSV)
+csvItself = open('everyone-master.csv', "r")
+parsedcsv = csv.reader(csvItself)
 
 # and here are the time vars
-  rightNow = datetime.datetime.now()
-  thisInstant = rightNow.strftime("%Y-%m-%d %H:%M:%S")
+rightNow = datetime.datetime.now()
+thisInstant = rightNow.strftime("%H:%M:%S")
+justToday = rightNow.strftime("%Y-%m-%d")
 
-# This one displays the username and date/time of clockin:
-# Here's where we loop through csv list
-  for row in parsedcsv:
-    # if current row's 1st value is equal to input, print that row
-      if cardNumber == row[0]:
-          print(row[1])
-      # and the time
-          print(thisInstant)
-      
+# this will take the parsed .csv above, compare the input number, and grab the name associated to it
+# so that it may append a document with the name of the numerical date with "Name, Timestamp"
+# it then clears the input 
+def theThing(): 
+      for row in parsedcsv:
+          # if current row's 1st value is equal to input, print that row
+          if cardNumber == row[0]:
+              window.Element('theName').Update(row[1])
+              window.Element('theTime').Update(thisInstant)
+              window.Element('IN').Update('')
+              with open('%s.csv' % justToday, 'a+') as f:
+                f.write(f'{row[1]},{thisInstant}\n')
 
-
-# here's how to find the number of the row of the given card number, 
-# userRowNumber will be plugged into whatever I can find to write the doc
-  def row_number(input):
-      o = open('everyone-master.csv', 'r') 
-      myData = csv.reader(o)
-      index = 0 
-      for row in myData:
-      #print row
-       if row[0] == input: 
-          return index 
-       else : index += 1
-  userRowNumber = row_number(cardNumber)
-  print(userRowNumber)
-
-while loopty :
-  prog()
+# mainloop:
+while True :
+      event, value = window.Read()
+      cardNumber = value['IN']
+      if event is None:
+        break 
+      elif event is 'RETURN':
+            theThing()
+      else:
+            print('whats going on here')
+        
+window.Close() 
