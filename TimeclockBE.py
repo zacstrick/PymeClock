349da39abaaea5c9ebf896b21af1a230d11c1acc
zacstrick <38014947+zacstrick@users.noreply.github.com>
2019-10-11 14:22:44 -0500

    Update TimeclockBE.py

--- TimeclockBE.py
+++ TimeclockBE.py
@@ -28,7 +28,6 @@ def submitToDB(EmpName, theDate, theTime):
     con.commit()
     con.close()
 
-
 def addEmp(EmpID, EmpName):
     con = sqlite3.connect('Timeclock.db')
     cur = con.cursor()
@@ -45,19 +44,110 @@ def confEmp(EmpID, EmpName):
     con.close()
     return confirmation
     
-
-def onTheDay(EmpName, theDate):
-    con=sqlite3.connect('Timeclock.db')
+def onTheDayButton(EmpName, theDate):
+    con = sqlite3.connect('Timeclock.db')
     cur = con.cursor()
     cur.execute("SELECT theTime FROM Swipes WHERE EmpName=? AND theDate=?", (EmpName, theDate))
     tupes = cur.fetchall()
     con.close()
     detuped = [x[0] for x in tupes]
+    num = len(detuped)
     FMT = '%H:%M:%S'
-    day = (dt.strptime(detuped[3], FMT) - dt.strptime(detuped[0], FMT)) 
-    lunch = (dt.strptime(detuped[2], FMT) - dt.strptime(detuped[1], FMT))
-    workday = day - lunch
-    return workday
+    ri = "\nYou're On the Clock!"
+    ro = "\nYou're Clocked Out!"
+    if num == 1:
+        sass = "You just got here.."
+        return sass
+
+    elif num == 2:
+        sofar = (dt.strptime(detuped[1], FMT) - dt.strptime(detuped[0], FMT))
+        sofarro = str(sofar) + ro
+        return sofarro
+    
+    elif num == 3:
+        sofar1 = (dt.strptime(detuped[1], FMT) - dt.strptime(detuped[0], FMT))
+        infromlunch = str(sofar1)+ri
+        return infromlunch
+    
+    elif num == 4:
+        day = (dt.strptime(detuped[3], FMT) - dt.strptime(detuped[0], FMT)) 
+        lunch = (dt.strptime(detuped[2], FMT) - dt.strptime(detuped[1], FMT))
+        workday = day - lunch
+        workday1 = str(workday)+ro
+        return workday1
+
+    elif num == 5:
+        day0 = (dt.strptime(detuped[3], FMT) - dt.strptime(detuped[0], FMT)) 
+        lunch0 = (dt.strptime(detuped[2], FMT) - dt.strptime(detuped[1], FMT))
+        workday0 = day0 - lunch0
+        stworkday = str(workday0)
+        rsass = "\nYou're back in again I guess."
+        somethin = stworkday + rsass
+        return somethin
+
+    elif num == 6:
+        day00 = (dt.strptime(detuped[5], FMT) - dt.strptime(detuped[0], FMT)) 
+        lunch00 = (dt.strptime(detuped[2], FMT) - dt.strptime(detuped[1], FMT))
+        lunch2 = (dt.strptime(detuped[4], FMT) - dt.strptime(detuped[3], FMT))
+        workday00 = day00 - (lunch00 + lunch2)
+        strworkday = str(workday00)
+        a = "\nok now you're leaving, right?"
+        itt = strworkday + a
+        return itt
+
+def onTheDayAuto(EmpName, theDate):
+    con = sqlite3.connect('Timeclock.db')
+    cur = con.cursor()
+    cur.execute("SELECT theTime FROM Swipes WHERE EmpName=? AND theDate=?", (EmpName, theDate))
+    tupes = cur.fetchall()
+    con.close()
+    detuped = [x[0] for x in tupes]
+    num = len(detuped)
+    FMT = '%H:%M:%S'
+    ri = "\nClocked In!"
+    ro = "\nClocked Out!"
+    if num == 1:
+        return ri
+
+    elif num == 2:
+        sofar = (dt.strptime(detuped[1], FMT) - dt.strptime(detuped[0], FMT))
+        outforlunch = str(sofar)+ro
+        return outforlunch
+    
+    elif num == 3:
+        sofar1 = (dt.strptime(detuped[1], FMT) - dt.strptime(detuped[0], FMT))
+        infromlunch = str(sofar1)+ri
+        return infromlunch
+    
+    elif num == 4:
+        day = (dt.strptime(detuped[3], FMT) - dt.strptime(detuped[0], FMT)) 
+        lunch = (dt.strptime(detuped[2], FMT) - dt.strptime(detuped[1], FMT))
+        workday = day - lunch
+        workday1 = str(workday)+ro
+        return workday1
+        
+    elif num == 5:
+        q = "Woah wait why are you back?"
+        return q
+
+    elif num == 6:
+        day00 = (dt.strptime(detuped[5], FMT) - dt.strptime(detuped[0], FMT)) 
+        lunch00 = (dt.strptime(detuped[2], FMT) - dt.strptime(detuped[1], FMT))
+        lunch2 = (dt.strptime(detuped[4], FMT) - dt.strptime(detuped[3], FMT))
+        workday00 = day00 - (lunch00 + lunch2)
+        strworkday = str(workday00)
+        a = "\nGood job today buddy."
+        bbb = strworkday + a
+        return bbb
+
+
+
+#def onTheWeek():
+    #find the dates since monday
+    #take values from each of those dates
+    #add them together
+    #voila
+
 
     
 def delEmp(EmpID):
